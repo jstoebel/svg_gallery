2f0e17e3c31ae71f4abdfd06bd817857c224ca06
Jacob Stoebel <jstoebel@gmail.com>
2019-04-24 18:25:23 -0400

    change to postgres and use ts with image model

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import {Sequelize} from 'sequelize-typescript';
-import Image from './models/Image'
-import logger from '../logger'
+import Image from 'models/Image'
+import logger from 'lib/logger';
 
 // help setting up postgres locally https://gist.github.com/ibraheem4/ce5ccd3e4d7a65589ce84f2a3b7c23a3
 
